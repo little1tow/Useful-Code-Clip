4b630dccaff1b3ee2a73780855a5ce90445a27e8
zhkun <little1tow@users.noreply.github.com>
2023-01-10 11:10:44 +0800

    add mixup, mean and std calculation, and label smoothing operation

--- 4-common_operation.py
+++ 4-common_operation.py
@@ -27,3 +27,156 @@ def tanh(x):
 def L2(yhat, y):
     loss = np.sum(np.multiply((y - yhat), (y - yhat)))
     return loss
+
+
+# Mixup operation
+beta_distribution = torch.distributions.beta.Beta(alpha, alpha)
+for images, labels in train_loader:
+    images, labels = images.cuda(), labels.cuda()
+
+    # Mixup images and labels.
+    lambda_ = beta_distribution.sample([]).item()
+    index = torch.randperm(images.size(0)).cuda()
+    mixed_images = lambda_ * images + (1 - lambda_) * images[index, :]
+    label_a, label_b = labels, labels[index]
+
+    # Mixup loss.
+    scores = model(mixed_images)
+    loss = (lambda_ * loss_function(scores, label_a)
+            + (1 - lambda_) * loss_function(scores, label_b))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+# calculate the mean and std
+import os
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+from PIL import Image
+
+def compute_mean_and_std(dataset):
+    # 输入PyTorch的dataset，输出均值和标准差
+    mean_r = 0
+    mean_g = 0
+    mean_b = 0
+
+    for img, _ in dataset:
+        img = np.asarray(img) # change PIL Image to numpy array
+        mean_b += np.mean(img[:, :, 0])
+        mean_g += np.mean(img[:, :, 1])
+        mean_r += np.mean(img[:, :, 2])
+
+    mean_b /= len(dataset)
+    mean_g /= len(dataset)
+    mean_r /= len(dataset)
+
+    diff_r = 0
+    diff_g = 0
+    diff_b = 0
+
+    N = 0
+
+    for img, _ in dataset:
+        img = np.asarray(img)
+
+        diff_b += np.sum(np.power(img[:, :, 0] - mean_b, 2))
+        diff_g += np.sum(np.power(img[:, :, 1] - mean_g, 2))
+        diff_r += np.sum(np.power(img[:, :, 2] - mean_r, 2))
+
+        N += np.prod(img[:, :, 0].shape)
+
+    std_b = np.sqrt(diff_b / N)
+    std_g = np.sqrt(diff_g / N)
+    std_r = np.sqrt(diff_r / N)
+
+    mean = (mean_b.item() / 255.0, mean_g.item() / 255.0, mean_r.item() / 255.0)
+    std = (std_b.item() / 255.0, std_g.item() / 255.0, std_r.item() / 255.0)
+    return mean, std
+
+
+# label smoothing operation
+import torch
+import torch.nn as nn
+
+class LSR(nn.Module):
+
+    def __init__(self, e=0.1, reduction='mean'):
+        super().__init__()
+
+        self.log_softmax = nn.LogSoftmax(dim=1)
+        self.e = e
+        self.reduction = reduction
+
+    def _one_hot(self, labels, classes, value=1):
+        """
+            Convert labels to one hot vectors
+
+        Args:
+            labels: torch tensor in format [label1, label2, label3, ...]
+            classes: int, number of classes
+            value: label value in one hot vector, default to 1
+
+        Returns:
+            return one hot format labels in shape [batchsize, classes]
+        """
+
+        one_hot = torch.zeros(labels.size(0), classes)
+
+        #labels and value_added  size must match
+        labels = labels.view(labels.size(0), -1)
+        value_added = torch.Tensor(labels.size(0), 1).fill_(value)
+
+        value_added = value_added.to(labels.device)
+        one_hot = one_hot.to(labels.device)
+
+        one_hot.scatter_add_(1, labels, value_added)
+
+        return one_hot
+
+    def _smooth_label(self, target, length, smooth_factor):
+        """convert targets to one-hot format, and smooth
+        them.
+        Args:
+            target: target in form with [label1, label2, label_batchsize]
+            length: length of one-hot format(number of classes)
+            smooth_factor: smooth factor for label smooth
+
+        Returns:
+            smoothed labels in one hot format
+        """
+        one_hot = self._one_hot(target, length, value=1 - smooth_factor)
+        one_hot += smooth_factor / (length - 1)
+
+        return one_hot.to(target.device)
+
+    def forward(self, x, target):
+
+        if x.size(0) != target.size(0):
+            raise ValueError('Expected input batchsize ({}) to match target batch_size({})'
+                    .format(x.size(0), target.size(0)))
+
+        if x.dim() < 2:
+            raise ValueError('Expected input tensor to have least 2 dimensions(got {})'
+                    .format(x.size(0)))
+
+        if x.dim() != 2:
+            raise ValueError('Only 2 dimension tensor are implemented, (got {})'
+                    .format(x.size()))
+
+        smoothed_target = self._smooth_label(target, x.size(1), self.e)
+        x = self.log_softmax(x)
+        loss = torch.sum(- x * smoothed_target, dim=1)
+
+        if self.reduction == 'none':
+            return loss
+
+        elif self.reduction == 'sum':
+            return torch.sum(loss)
+
+        elif self.reduction == 'mean':
+            return torch.mean(loss)
+
+        else:
+            raise ValueError('unrecognized option, expect reduction to be one of none, mean, sum')
